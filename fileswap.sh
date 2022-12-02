10c84fbf73e47fb1c009bf85d7d9580deede8151
Kempegowda <Kempegowdamsrit@gmail.com>
2022-12-02 05:11:51 +0000

    modifing fileswap

--- fileswap.sh
+++ fileswap.sh
@@ -2,6 +2,7 @@
 linecount1=$(wc -l abc.txt | cut -d\  -f 1 )
 linecount2=$(wc -l xyz.txt | cut -d\  -f 1 )
 i=1 j=1
+
 while [ $i -le $linecount1 -a $j -le $linecount2 ]; 
 do
     sed -n -e "${i}p" abc.txt >> output.txt
@@ -9,6 +10,7 @@ do
     echo "-----" >> output.txt
     i=$(( i + 1 )) 
     j=$(( j + 1))
+
 done
 cat output.txt
 rm output.txt
